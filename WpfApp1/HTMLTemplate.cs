8beab94172057117baf95de21cd45cb44d8af45d
Rick Tillery <github@tilleryweb.com>
2017-10-08 22:07:48 -0500

    More work

---
+++ WpfApp1/HTMLTemplate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.IO;
+
+namespace WpfApp1
+{
+    public class HTMLTemplate
+    {
+        static public void Fill(String templateFilename, String htmlFilename_, NameValueCollection pairs)
+        {
+            String content;
+            using (var input = new StreamReader(templateFilename))
+            {
+                content = input.ReadToEnd();
+            }
+            foreach (var key in pairs.AllKeys)
+            {
+                content = content.Replace("[" + key + "]", pairs[key]);
+            }
+            using (var output = new StreamWriter(htmlFilename_))
+            {
+                output.Write(content);
+            }
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Specialized;
+using System.Windows;
+using System.Drawing;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        private void UpdateHTML()
+        {
+            NameValueCollection pairs = new NameValueCollection()
+            {
+                { "HOMESHORT", this.textBoxHomeShort.Text },
+                { "VISITORSHORT", this.textBoxVisitorShort.Text },
+                { "HOMEBKGND", colorHomeBkgnd.ToString() },
+                { "HOMEFOREGND", colorHomeForegnd.ToString() },
+                { "VISITORBKGND", colorVisitorBkgnd.ToString() },
+                { "VISITORFOREGND", colorVisitorForegnd.ToString() },
+                { "VISITORSCORE", (string)this.labelBoxVisitorTotalRuns.Content },
+                { "HOMESCORE", (string)this.labelBoxHomeTotalRuns.Content },
+                { "BALLS", (string)this.labelCurrentBalls.Content },
+                { "STRIKES", (string)this.labelCurrentStrikes.Content },
+                { "OUTS", (string)this.labelCurrentOuts.Content },
+                { "TOPBOTINNING", this.radioButtonTop.IsChecked == true ? "Top" : "Bot" }, /* TODO - Generic */
+                { "INNING", (string)this.labelCurrentInning.Content }
+            };
+            HTMLTemplate.Fill("C:\\Projects\\WindowsFormsApp1\\WindowsFormsApp1\\bug1-template.html",
+                              "C:\\Projects\\WindowsFormsApp1\\WindowsFormsApp1\\bug1.html",
+                              pairs);
+        }
+    }
+}
