d9b3a364866f5ba6f6b1c6fca2d0051d11def34b
Rick Tillery <github@tilleryweb.com>
2017-10-09 22:19:49 -0500

    Base color

--- WpfApp1/MainWindow.cs
+++ WpfApp1/MainWindow.cs
@@ -11,23 +11,31 @@ namespace WpfApp1
             return "rgba(" + color.R + "," + color.G + "," + color.B + "," + color.A / 255.0 + ")";
         }
 
+        private System.Windows.Media.Color GetBaseColor()
+        {
+            return (radioButtonTop.IsChecked == true) ?
+                    ((radioButtonVisitorFillBkgnd.IsChecked == true) ? colorVisitorBkgnd : colorVisitorForegnd) :
+                    ((radioButtonHomeFillBkgnd.IsChecked == true) ? colorHomeBkgnd : colorHomeForegnd);
+        }
+
         private void UpdateHTML()
         {
             NameValueCollection pairs = new NameValueCollection()
             {
-                { "HOMESHORT", this.textBoxHomeShort.Text },
-                { "VISITORSHORT", this.textBoxVisitorShort.Text },
+                { "HOMESHORT", textBoxHomeShort.Text },
+                { "VISITORSHORT", textBoxVisitorShort.Text },
                 { "HOMEBKGND", ColorToHTML(colorHomeBkgnd) },
                 { "HOMEFOREGND", ColorToHTML(colorHomeForegnd) },
                 { "VISITORBKGND", ColorToHTML(colorVisitorBkgnd) },
                 { "VISITORFOREGND", ColorToHTML(colorVisitorForegnd) },
-                { "VISITORSCORE", (string)this.labelBoxVisitorTotalRuns.Content },
-                { "HOMESCORE", (string)this.labelBoxHomeTotalRuns.Content },
-                { "BALLS", (string)this.labelCurrentBalls.Content },
-                { "STRIKES", (string)this.labelCurrentStrikes.Content },
-                { "OUTS", (string)this.labelCurrentOuts.Content },
-                { "TOPBOTINNING", this.radioButtonTop.IsChecked == true ? "Top" : "Bot" }, /* TODO - Generic */
-                { "INNING", (string)this.labelCurrentInning.Content }
+                { "VISITORSCORE", (string)labelBoxVisitorTotalRuns.Content },
+                { "HOMESCORE", (string)labelBoxHomeTotalRuns.Content },
+                { "BALLS", (string)labelCurrentBalls.Content },
+                { "STRIKES", (string)labelCurrentStrikes.Content },
+                { "OUTS", (string)labelCurrentOuts.Content },
+                { "TOPBOTINNING", radioButtonTop.IsChecked == true ? "Top" : "Bot" }, /* TODO - Generic */
+                { "INNING", (string)labelCurrentInning.Content },
+                { "ATBATCOLOR", ColorToHTML(GetBaseColor()) }
             };
             HTMLTemplate.Fill("C:\\Projects\\WindowsFormsApp1\\WindowsFormsApp1\\bug1-template.html",
                               "C:\\Projects\\WindowsFormsApp1\\WindowsFormsApp1\\bug1.html",
