97af2700eed7eeee6725a7a7deb0179c438bd641
Rick Tillery <github@tilleryweb.com>
2017-11-02 22:49:33 -0500

    Move HTML subst

--- WpfApp1/MainWindow.cs
+++ WpfApp1/MainWindow.cs
@@ -19,32 +19,58 @@ namespace WpfApp1
                     ((radioButtonHomeFillBkgnd.IsChecked == true) ? colorHomeBkgnd : colorHomeForegnd);
         }
 
+        private NameValueCollection pairs = new NameValueCollection();
+
+        private void InitHTMLMapping()
+        {
+            pairs["HOMELONG"] = textBoxHomeLong.Text;
+            pairs["HOMESHORT"] = textBoxHomeShort.Text;
+            pairs["HOMEBKGND"] = ColorToHTML(colorHomeBkgnd);
+            pairs["HOMEFOREGND"] = ColorToHTML(colorHomeForegnd);
+            pairs["HOMEHITS"] = (string)labelBoxHomeTotalHits.Content;
+            pairs["HOMEERRORS"] = (string)labelBoxHomeTotalErrors.Content;
+            pairs["HOMESCORE"] = (string)labelBoxHomeTotalRuns.Content;
+
+            pairs["VISITORLONG"] = textBoxVisitorLong.Text;
+            pairs["VISITORSHORT"] = textBoxVisitorShort.Text;
+            pairs["VISITORBKGND"] = ColorToHTML(colorVisitorBkgnd);
+            pairs["VISITORFOREGND"] = ColorToHTML(colorVisitorForegnd);
+            pairs["VISITORHITS"] = (string)labelBoxVisitorTotalHits.Content;
+            pairs["VISITORERRORS"] = (string)labelBoxVisitorTotalErrors.Content;
+            pairs["VISITORSCORE"] = (string)labelBoxVisitorTotalRuns.Content;
+
+            pairs["INNING"] = (string)labelCurrentInning.Content;
+            pairs["TOPVISIBLE"] = radioButtonTop.IsChecked == true ? "visible" : "hidden";
+            pairs["TOPDISPLAY"] = radioButtonTop.IsChecked == true ? "inherit" : "none";
+            pairs["BOTTOMVISIBLE"] = radioButtonTop.IsChecked == true ? "hidden" : "visible";
+            pairs["BOTTOMDISPLAY"] = radioButtonTop.IsChecked == true ? "none" : "inherit";
+            pairs["ATBATCOLOR"] = ColorToHTML(GetBaseColor());
+
+            pairs["BALLS"] = (string)labelCurrentBalls.Content;
+            pairs["STRIKES"] = (string)labelCurrentStrikes.Content;
+            pairs["OUTS"] = (string)labelCurrentOuts.Content;
+
+            pairs["1STBASEVISIBILITY"] = toggle1stBase.IsChecked == true ? "visible" : "hidden";
+            pairs["2NDBASEVISIBILITY"] = toggle2ndBase.IsChecked == true ? "visible" : "hidden";
+            pairs["3RDBASEVISIBILITY"] = toggle3rdBase.IsChecked == true ? "visible" : "hidden";
+
+            pairs["MAXINNINGS"] = MAXINNING.ToString();
+
+            int inning = int.Parse((string)labelCurrentInning.Content);
+            for (int i = 0; i < MAXINNING; i++)
+            {
+                pairs["HOME" + i] = (string)labelBoxInningHomeRuns[i].Content;
+                pairs["VISITOR" + i] = (string)labelBoxInningVisitorRuns[i].Content;
+                pairs["INNING" + i + "VISIBILITY"] = inning > i ? "visible" : "hidden";
+                pairs["INNING" + i + "DISPLAY"] = inning > i ? "inherit" : "none";
+            }
+        }
+
         private void UpdateHTML()
         {
-            NameValueCollection pairs = new NameValueCollection()
-            {
-                { "HOMESHORT", textBoxHomeShort.Text },
-                { "VISITORSHORT", textBoxVisitorShort.Text },
-                { "HOMEBKGND", ColorToHTML(colorHomeBkgnd) },
-                { "HOMEFOREGND", ColorToHTML(colorHomeForegnd) },
-                { "VISITORBKGND", ColorToHTML(colorVisitorBkgnd) },
-                { "VISITORFOREGND", ColorToHTML(colorVisitorForegnd) },
-                { "VISITORSCORE", (string)labelBoxVisitorTotalRuns.Content },
-                { "HOMESCORE", (string)labelBoxHomeTotalRuns.Content },
-                { "BALLS", (string)labelCurrentBalls.Content },
-                { "STRIKES", (string)labelCurrentStrikes.Content },
-                { "OUTS", (string)labelCurrentOuts.Content },
-                { "TOPVISIBLE", radioButtonTop.IsChecked == true ? "visible" : "hidden" },
-                { "BOTTOMVISIBLE", radioButtonTop.IsChecked == true ? "hidden" : "visible" },
-                { "TOPDISPLAY", radioButtonTop.IsChecked == true ? "inherit" : "none" },
-                { "BOTTOMDISPLAY", radioButtonTop.IsChecked == true ? "none" : "inherit" },
-                { "INNING", (string)labelCurrentInning.Content },
-                { "ATBATCOLOR", ColorToHTML(GetBaseColor()) },
-                { "1STBASEVISIBILITY", toggle1stBase.IsChecked == true ? "visible" : "hidden" },
-                { "2NDBASEVISIBILITY", toggle2ndBase.IsChecked == true ? "visible" : "hidden" },
-                { "3RDBASEVISIBILITY", toggle3rdBase.IsChecked == true ? "visible" : "hidden" },
-            };
-            foreach(var item in listboxTemplates.Items)
+            InitHTMLMapping();
+
+            foreach (var item in listboxTemplates.Items)
             {
                 var template = item.ToString();
                 var filename = Path.GetFileName(template);
